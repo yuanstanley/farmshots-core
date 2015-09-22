4ab29cfff1ca25fec0eab7181f2c5818f86e11b7
Ouwen <ouwen.huang@hotmail.com>
2015-09-22 12:46:04 -0400

    Initial Config.
    Done:
     - Grunt is setup
     - Cors is enabled
    
    Todo:
     - Auto deploy to ec2
     - Decide on need for prod vs dev

---
+++ server/routes.js
@@ -0,0 +1,16 @@
+/**
+ * Main application routes
+ */
+
+'use strict';
+
+var errors = require('./components/errors');
+
+module.exports = function(app) {
+
+  // Insert routes below
+  app.use('/api/things', require('./api/thing'));
+  app.use('/api/users', require('./api/user'));
+
+  app.use('/auth', require('./auth'));
+};
